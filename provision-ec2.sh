8242d768d731fd15264b2a8a4ba7d75d8883158e
danasmera <daniel@linuxfreelancer.com>
2015-12-29 12:45:05 -0500

    Allow IP address update

--- provision-ec2.sh
+++ provision-ec2.sh
@@ -6,19 +6,21 @@ ARGCOUNT="$#"
 SKIP=0
 
 function Usage {
-echo "Usage: $(basename $0) -o LINUX_DISTRO_NAME[centos|ubuntu] -i IPADDRESS"
+echo "Usage: $(basename $0) -o LINUX_DISTRO_NAME[rhel|ubuntu] -i IPADDRESS"
 exit 1
 }
 
 function updateinput {
-:
+read -p "IP address: " newipaddress
+export IPADDRESS=$newipaddress
+return 0
 }
 if [ "$ARGCOUNT" -eq 0 ]; then
   MYDISTRO=${MYDISTRO-"ubuntu"}
   IMAGEID=${IMAGEID-"ami-d05e75b8"}
   MYUSER=${MYUSER-"ubuntu"}
   IPADDRESS=${MANPATH-"192.168.2.9"}
-  MYPLAYBOOK='provision_ubuntu.yml'
+  MYPLAYBOOK='breakme_ubuntu.yml'
   SKIP=1
   else
   IPADDRESS=${MANPATH-"192.168.2.9"}
@@ -30,7 +32,7 @@ if [ "$SKIP" -ne 1 ]; then
   case $opt in
     o)
        MYDISTRO=$OPTARG
-       echo "${MYDISTRO}" | grep -q -i -P '(\s+)?(centos|rhel|redhat|ubuntu)(\s+)?$'
+       echo "${MYDISTRO}" | grep -q -i -P '(\s+)?(rhel|redhat|ubuntu)(\s+)?$'
        if [ "$?" -ne 0 ]; then Usage; fi
        ;;
     i)
@@ -48,10 +50,10 @@ if [ "$SKIP" -ne 1 ]; then
   done
 fi
 ## If RHEL, let us set set the image ID and user name
-echo "$MYDISTRO" | grep -q -i -P '(\s+)?(centos|rhel|redhat)(\s+)?$' 
-[[ $? -eq 0 ]] && { IMAGEID='ami-90248af8' ; MYUSER='cloud-user'; MYPLAYBOOK='provision_centos.yml' ; }
+echo "$MYDISTRO" | grep -q -i -P '(\s+)?(rhel|redhat)(\s+)?$' 
+[[ $? -eq 0 ]] && { IMAGEID='ami-90248af8' ; MYUSER='cloud-user'; MYPLAYBOOK='breakme_rhel.yml' ; }
 echo "$MYDISTRO" | grep -q -i -P '(\s+)?(ubuntu)(\s+)?$'
-[[ $? -eq 0 ]] && { IMAGEID='ami-d05e75b8' ; MYUSER='ubuntu' ; MYPLAYBOOK='provision_ubuntu.yml' ;}
+[[ $? -eq 0 ]] && { IMAGEID='ami-d05e75b8' ; MYUSER='ubuntu' ; MYPLAYBOOK='breakme_ubuntu.yml' ;}
 ## Provision
 echo "We will provision an EC2 with the following specification."
 echo "Linux Distro: $MYDISTRO"
@@ -66,13 +68,13 @@ if [ "$response" = 'Y' -o "$response" = 'YES' ]; then
   echo "Provisioning in progress .... "
 elif [ "$response" = 'U' -o "$response" = 'UPDATE' ]; then
   updateinput
-  exit 1
+  echo $IPADDRESS 
 else
   echo "Interrupted provisioning."
   exit 1
 fi
 ##vars
-PRIVKEY='aws.pem'
+PRIVKEY='rhce.pem'
 
 export imageid="$IMAGEID"
 export private_ip="$IPADDRESS"
